217603d1aeefe8800717c01a1c2cd25e285e882e
Rohan Kadam <kadamrohan76@gmail.com>
2020-06-13 01:01:13 +0530

    fetch api usage

--- public/client.js
+++ public/client.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
 console.log("DOM is loaded");
+
+$('#showWeather').on('click',()=>{
+    navigator.geolocation.getCurrentPosition((positon)=>{
+        console.log(positon);
+        $('#weather_info').text(positon.coords.latitude+" "+positon.coords.longitude);
+var latitude=positon.coords.latitude;
+var longitude=positon.coords.longitude;
+        fetch(`https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&&lon=${longitude}`)
+        .then(response=>response.json())
+        .then(data=>{   
+console.log(data);
+
+        })
+        .catch(err=>{
+console.log(err);
+        })
+                    },()=>{
+        console.log("Error in fetching");
+                    });
+});
+
+
 });
