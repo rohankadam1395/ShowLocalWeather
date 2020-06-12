b9c5f342c8bb712749b7e09cd793ddf045a72a5b
Rohan Kadam <kadamrohan76@gmail.com>
2020-06-12 23:11:14 +0530

    exprees.static jquery CdnOnPage&client.js on serve

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-
+app.use('/public',express.static(process.cwd()+'/public'));
 app.get('/',(req,res)=>{
     res.sendFile(process.cwd()+'/views/index.html');
 });
