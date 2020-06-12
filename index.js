680caa034ae9f9d5fddfb2c166e7a43168bddd42
Rohan Kadam <kadamrohan76@gmail.com>
2020-06-12 18:42:12 +0530

    Using res.sendFile o send html file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 
 app.get('/',(req,res)=>{
-    res.send("Hello World");
+    res.sendFile(process.cwd()+'/views/index.html');
 });
 
 app.listen(8085,()=>{
