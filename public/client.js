97d9c7fbe1fc7a3cdfb2861b68f4e64b893a63eb
Rohan Kadam <kadamrohan76@gmail.com>
2020-06-13 12:37:33 +0530

    Temp Conversion and  conditional display in html

--- public/client.js
+++ public/client.js
@@ -12,6 +12,38 @@ var longitude=positon.coords.longitude;
         .then(data=>{   
 console.log(data);
 
+$('#weather_image').attr('src',data.weather[0].icon);
+$('#temperature #temperature_value').text(data.main.temp);
+$('#temperature #temperature_symbol').text("  "+String.fromCharCode(176));
+$('#temperature #temperature_unit').text("C");
+$('#location').text(data.name+","+data.sys.country);
+$('#temperature #temperature_unit').on('click',(event)=>{
+console.log(event.target.innerText);
+var unit=event.target.innerText;
+var temp=parseFloat($('#temperature #temperature_value').text());
+console.log(temp);
+
+if(unit.localeCompare("C")==0){
+console.log("Its a C");
+$('#temperature #temperature_unit').text("F");
+temp=(temp*(9/5)+32).toFixed(2);
+$('#temperature #temperature_value').text(temp);
+
+}else{
+        $('#temperature #temperature_unit').text("C");  
+
+        console.log("May be F");
+        temp=((temp-32)*(5/9)).toFixed(2);
+        $('#temperature #temperature_value').text(temp);
+
+
+}
+
+
+
+});
+
+
         })
         .catch(err=>{
 console.log(err);
