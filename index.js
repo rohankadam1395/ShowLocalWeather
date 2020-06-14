d56dd9967bd1e85765df4611fad8afbf43828b7b
Rohan Kadam <kadamrohan76@gmail.com>
2020-06-14 09:10:02 +0530

    Chnaged port to work for local and heroku both

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express=require('express');
 const app=express();
+let port=process.env.PORT;
+if(port==null||port==""){
+port=8085;
+}
 app.use('/public',express.static(process.cwd()+'/public'));
 app.get('/',(req,res)=>{
     res.sendFile(process.cwd()+'/views/index.html');
 });
 
-app.listen(8085,()=>{
-    console.log("Listening on port 8080");
+app.listen(port,()=>{
+    console.log("Listening on port 8085");
 });
